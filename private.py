cdf0b412d4aeed7959e8fd7c6c45dbe65b56a673
Asliasghar Soltani <alisoltanics@gmail.com>
2019-02-01 09:46:26 +0330

    Add private.py file

---
+++ private.py
@@ -0,0 +1,9 @@
+class Book:
+    def __init__(self, name):
+        self.name = name
+        self._author = None
+
+book_object = Book("secret key")
+# for making a variable private we can use `_` but still we can access to it. it's a convention
+print(book_object._author)
+        
