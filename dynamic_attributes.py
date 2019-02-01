c1d15bfcb371f9677c21372bf3057704d20b6ea0
Asliasghar Soltani <alisoltanics@gmail.com>
2019-02-01 14:26:31 +0330

    Add dynamic_attributes.py file

---
+++ dynamic_attributes.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __getattr__(self, attr):
+        if attr.startswith("ali"):
+            return "you know me."
+        raise AttributeError(
+            "{} has no attribute {}".format(self.__class__.__name__, attr)
+        )
+
+person = Person("ali")
+print(person.ali_asghar)
