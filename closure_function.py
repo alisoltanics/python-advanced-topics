1a5bb144417653291c3e3d094b1f8f1aaa638d09
Asliasghar Soltani <alisoltanics@gmail.com>
2021-01-23 10:29:17 +0330

    add closure

---
+++ closure_function.py
@@ -0,0 +1,24 @@
+def print_msg(msg):
+    # This is the outer enclosing function
+
+    def printer():
+        # This is the nested function
+        print(msg)
+
+    return printer  # returns the nested function
+
+
+# Now let's try calling this function.
+# Output: Hello
+another = print_msg("Hello")
+another()
+
+Output:
+Hello
+
+
+"""
+The print_msg() function was called with the string "Hello" and the returned function was bound to the name another. On calling another(), the message was still remembered although we had already finished executing the print_msg() function.
+
+This technique by which some data ("Hello in this case) gets attached to the code is called closure in Python.
+"""
