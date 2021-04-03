04a636a125adc309812de8439d37e2d945043808
Asliasghar Soltani <alisoltanics@gmail.com>
2021-04-03 14:55:22 +0430

    change getitem_setitem.py

--- getitem_setitem.py
+++ getitem_setitem.py
@@ -1,3 +1,9 @@
+__setitem__ is a method used for assigning a value to an item. It is implicitly
+invoked when we set a value to an item of a list, dictionary, etc. __getitem__ is
+a method used for getting the value of an item. It is implicitly invoked when we access
+the items of a list, dictionary, etc. We can overload their operations by explicitly defining
+them.
+
 class Counter():
     def __init__(self, floors):
         self._floors = [None]*floors
@@ -16,3 +22,7 @@ index[2] = 'IJKL'
 index[3] = 'MNOP'
 
 print(index[2])
+
+
+
+https://www.codespeedy.com/__setitem__-and-__getitem__-in-python-with-example/#:~:text=Both%20__setitem__%20and,generally%20used%20for%20operator%20overloading.
