46e1052cfa2ad2c3838ae1c1c5a3bd2b59adf434
Asliasghar Soltani <alisoltanics@gmail.com>
2021-04-03 15:04:50 +0430

    change private

--- private.py
+++ private.py
@@ -3,6 +3,16 @@ class Book:
         self.name = name
         self._author = None
 
+    # Right usage of private variable
+    @property
+    def author(self):
+        return self._author
+
+
 book_object = Book("secret key")
+
+
 # for making a variable private we can use `_` but still we can access to it. it's a convention
+
+# Don't do this
 print(book_object._author)
